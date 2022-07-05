82f75995ca28a8ea8c4a8a0bdbc79528bd719280
Sami Zakir Ahmed <54m1z4k@gmail.com>
2022-07-05 13:07:03 +0100

    Added option to change bar colours

--- 13-21-200 EMAs/13-21-200 EMAs.cs
+++ 13-21-200 EMAs/13-21-200 EMAs.cs
@@ -1,12 +1,12 @@
-using cAlgo.API;
+﻿using cAlgo.API;
 using cAlgo.API.Indicators;
- 
+
 namespace cAlgo
 {
     [Indicator(IsOverlay = true, AutoRescale = false, TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
     public class ThreeMAs : Indicator
     {
- 
+
         #region Parameters
         [Parameter("Source")]
         public DataSeries DataSource { get; set; }
@@ -22,24 +22,30 @@ namespace cAlgo
 
         [Parameter(name: "MA Period 1", Group = "MA Periods", DefaultValue = 13, MinValue = 1)]
         public int Period1 { get; set; }
- 
+
         [Parameter(name: "MA Period 2", Group = "MA Periods", DefaultValue = 21, MinValue = 1)]
         public int Period2 { get; set; }
- 
+
         [Parameter(name: "MA Period 3", Group = "MA Periods", DefaultValue = 200, MinValue = 1)]
         public int Period3 { get; set; }
+
+        [Parameter(name: "Bearish", Group = "Bar Colours", DefaultValue = "OrangeRed")]
+        public string BearishColour { get; set; }
+
+        [Parameter(name: "Bullish", Group = "Bar Colours", DefaultValue = "Green")]
+        public string BullishColour { get; set; }
         #endregion
 
         #region Outputs
         [Output("MA 0", LineColor = "Gray", Thickness = 2)]
         public IndicatorDataSeries Ma0 { get; set; }
- 
+
         [Output("MA 1", LineColor = "Green", Thickness = 2)]
         public IndicatorDataSeries Ma1 { get; set; }
- 
+
         [Output("MA 2", LineColor = "Yellow", Thickness = 2)]
         public IndicatorDataSeries Ma2 { get; set; }
- 
+
         [Output("MA 3", LineColor = "Red", Thickness = 2)]
         public IndicatorDataSeries Ma3 { get; set; }
         #endregion
@@ -48,7 +54,7 @@ namespace cAlgo
         private MovingAverage _ma1;
         private MovingAverage _ma2;
         private MovingAverage _ma3;
- 
+
         protected override void Initialize()
         {
             _series = MarketData.GetBars(Timeframe);
@@ -56,7 +62,7 @@ namespace cAlgo
             _ma2 = Indicators.MovingAverage(_series.ClosePrices, Period2, MaType);
             _ma3 = Indicators.MovingAverage(_series.ClosePrices, Period3, MaType);
         }
- 
+
         public override void Calculate(int index)
         {
             int timeframeIndex = _series.OpenTimes.GetIndexByTime(Bars.OpenTimes[index]);
@@ -66,8 +72,9 @@ namespace cAlgo
             Ma3[index] = _ma3.Result[timeframeIndex];
             Ma0[index] = 0.5 * (Ma1[index] + Ma2[index]);
 
-            if (!ChangeBarCol) return;
-            Color barColour = Ma1[index] > Ma2[index] ? Color.Green : Color.OrangeRed;
+            if (!ChangeBarCol)
+                return;
+            Color barColour = Ma1[index] > Ma2[index] ? BullishColour.ToString() : BearishColour.ToString();
             Chart.SetBarColor(index, barColour);
         }
     }
